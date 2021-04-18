43d0cfb3acc4fd95732569ecf00929c62146de64
andres-tg <andresucv2017@gmail.com>
2021-04-18 16:07:26 -0400

    resueltos los errores previos

---
+++ src/js/component/Footerr.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const footer = () => {
+	return (
+		<footer className="bg-dark text-center text-white">
+			<div
+				className="text-center p-3"
+				style="background-color: rgba(0, 0, 0, 0.2);">
+				© 2021 Copyright:
+				<a className="text-white">Mi página que tal.</a>
+			</div>
+		</footer>
+	);
+};
